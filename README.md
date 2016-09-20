44b4c5d4319dd72a46dd4404af15327c3fcf5f8e
valoriedodge <valoriejones@gmail.com>
2016-09-19 21:56:43 -0700

    Readme Updated

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# address-bloc
+# Address Book
+
+This is a basic address book service built with Ruby that lets you browse entries, read and enter contacts from a CSV file, or add/edit individual contacts.
+
+````
+puts "Main Menu "
+puts "1 - View all entries"
+puts "2 - Create an entry"
+puts "3 - Search for an entry"
+puts "4 - Import entries from a CSV"
+puts "5 - Exit"
+print "Enter your selection: "
+````
--- models/address_book.rb
+++ models/address_book.rb
@@ -3,43 +3,43 @@ require "csv"
 
 class AddressBook
     attr_reader :entries
-    
+
     def initialize
         @entries = []
-        end
-    
+    end
+
     def add_entry(name, phone_number, email)
-        
+
         index = 0
         entries.each do |entry|
             if name < entry.name
                 break
-                end
-            index +=1
             end
-        entries.insert(index, Entry.new(name, phone_number, email))
+            index +=1
         end
-    
+        entries.insert(index, Entry.new(name, phone_number, email))
+    end
+
     def import_from_csv(file_name)
-        
+
         csv_text = File.read(file_name)
         csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
-        
+
         csv.each do |row|
             row_hash = row.to_hash
             add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
-            end
-        
         end
-    
+
+    end
+
     def binary_search(name)
         lower = 0
         upper = entries.length-1
-        
+
         while lower <= upper
             mid = (lower + upper)/2
             mid_name = entries[mid].name
-            
+
             if name == mid_name
                 return entries[mid]
             elsif name < mid_name
@@ -48,10 +48,10 @@ class AddressBook
                 lower = mid+1
             end
         end
-        
+
         return nil
-        
-        end
-        
+
+    end
+
 
 end
